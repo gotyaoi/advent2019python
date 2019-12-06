cd9ea5ebabf9b319168890c14cd817e7234f7a23
gotyaoi <gotyaoi@hotmail.com>
2019-12-05 16:15:44 -0800

    Centralizes opcode and parameter decoding in intcode_v2.

--- intcode.py
+++ intcode.py
@@ -19,90 +19,37 @@ def intcode_v2(i):
     i = i[:]
     pc = 0
     while True:
-        code = i[pc]
-        opcode = code % 100
+        opcode, parameters = decode(i, pc)
         if opcode == 1:
-            if (code // 100) % 10:
-                parameter1 = i[pc+1]
-            else:
-                parameter1 = i[i[pc+1]]
-            if (code // 1000) % 10:
-                parameter2 = i[pc+2]
-            else:
-                parameter2 = i[i[pc+2]]
-            i[i[pc+3]] = parameter1 + parameter2
+            i[i[pc+3]] = parameters[0] + parameters[1]
             pc += 4
         elif opcode == 2:
-            if (code // 100) % 10:
-                parameter1 = i[pc+1]
-            else:
-                parameter1 = i[i[pc+1]]
-            if (code // 1000) % 10:
-                parameter2 = i[pc+2]
-            else:
-                parameter2 = i[i[pc+2]]
-            i[i[pc+3]] = parameter1 * parameter2
+            i[i[pc+3]] = parameters[0] * parameters[1]
             pc += 4
         elif opcode == 3:
             i[i[pc+1]] = int(input())
             pc += 2
         elif opcode == 4:
-            if (code // 100) % 10:
-                parameter1 = i[pc+1]
-            else:
-                parameter1 = i[i[pc+1]]
-            print(parameter1)
+            print(parameters[0])
             pc += 2
         elif opcode == 5:
-            if (code // 100) % 10:
-                parameter1 = i[pc+1]
-            else:
-                parameter1 = i[i[pc+1]]
-            if parameter1 != 0:
-                if (code // 1000) % 10:
-                    parameter2 = i[pc+2]
-                else:
-                    parameter2 = i[i[pc+2]]
-                pc = parameter2
+            if parameters[0] != 0:
+                pc = parameters[1]
             else:
                 pc += 3
         elif opcode == 6:
-            if (code // 100) % 10:
-                parameter1 = i[pc+1]
-            else:
-                parameter1 = i[i[pc+1]]
-            if parameter1 == 0:
-                if (code // 1000) % 10:
-                    parameter2 = i[pc+2]
-                else:
-                    parameter2 = i[i[pc+2]]
-                pc = parameter2
+            if parameters[0] == 0:
+                pc = parameters[1]
             else:
                 pc += 3
         elif opcode == 7:
-            if (code // 100) % 10:
-                parameter1 = i[pc+1]
-            else:
-                parameter1 = i[i[pc+1]]
-            if (code // 1000) % 10:
-                parameter2 = i[pc+2]
-            else:
-                parameter2 = i[i[pc+2]]
-            if parameter1 < parameter2:
+            if parameters[0] < parameters[1]:
                 i[i[pc+3]] = 1
             else:
                 i[i[pc+3]] = 0
             pc += 4
         elif opcode == 8:
-            if (code // 100) % 10:
-                parameter1 = i[pc+1]
-            else:
-                parameter1 = i[i[pc+1]]
-            if (code // 1000) % 10:
-                parameter2 = i[pc+2]
-            else:
-                parameter2 = i[i[pc+2]]
-            if parameter1 == parameter2:
+            if parameters[0] == parameters[1]:
                 i[i[pc+3]] = 1
             else:
                 i[i[pc+3]] = 0
@@ -112,3 +59,22 @@ def intcode_v2(i):
         else:
             raise ValueError
     return i[0]
+
+NUM_PARAMS = {1: 2, 2: 2,
+              3: 0, 4: 1,
+              5: 2, 6: 2,
+              7: 2, 8: 2,
+              99: 0}
+
+def decode(i, pc):
+    code = i[pc]
+    opcode = code % 100
+    num = NUM_PARAMS[opcode]
+    parameters = []
+    for j in range(1, num+1):
+        pos = 10 * 10**j
+        if (code // pos) % 10:
+            parameters.append(i[pc+j])
+        else:
+            parameters.append(i[i[pc+j]])
+    return opcode, parameters
